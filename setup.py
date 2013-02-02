98c6359d08bcf0cecfc1072753df8d38babe74dc
Vittorio Zamboni <zamboni@appliedgenomics.org>
2013-02-02 16:49:37 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='iga-python-dcr',
+    version='0.0.1',
+    description="Python access to DCR files",
+    author='Vittorio Zamboni',
+    author_email='zamboni@appliedgenomics.org',
+    license='MIT',
+    url='http://bitbucket.org/iga/python-dcr',
+    packages=[
+        'python-dcr',
+    ],
+)
