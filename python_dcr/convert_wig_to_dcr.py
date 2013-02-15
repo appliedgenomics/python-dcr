08b3fcd65ae74f002563060b4ce460af2380a358
hg <none@none>
2013-02-15 17:40:51 +0100

    Changed converter

--- python_dcr/convert_wig_to_dcr.py
+++ python_dcr/convert_wig_to_dcr.py
@@ -24,6 +24,12 @@ def convert_wig_to_dcr(wig, dcr, chunk_size=1000, **kwargs):
     types = kwargs.get('types', 'int')
     null_value = kwargs.get('null_value', 0)
 
+    conv = str
+    if types == 'int':
+        conv = int
+    elif types == 'float':
+        conv = float
+
     ih = open(wig)
     oh = open(dcr, 'w')
     ohi = open(dcr + '.dcri', 'w')
@@ -43,19 +49,19 @@ def convert_wig_to_dcr(wig, dcr, chunk_size=1000, **kwargs):
     chrom = ''
     chrom_lines = 1
     chunk_values = []
+    info = {}
 
     for line in ih:
         line = line.strip()
         if line != '' and not line.startswith('track'):
             if 'chrom' in line:
                 if chunk_values:
-                    first_num = (chrom_lines - 1) * chunk_size + 1
-                    last_num = first_num + len(chunk_values) - 1
-                    oh.write('\n%s\t%s\t%s\t' % (chrom, first_num, last_num))
-                    for value in chunk_values[:-1]:
-                        oh.write('%s ' % value)
-                    oh.write('%s' % chunk_values[-1])
-                    lines += 1
+                    info = collect_info(info, line, conv)
+                    chrom_lines, chunk_values, lines, info = \
+                            process_value(chrom_lines, chunk_values,
+                                          lines, info, oh)
+                if 'chrom' in info:
+                    write_index(info, ohi)
 
                 ''' Get header '''
                 values = line.split(' ')
@@ -68,69 +74,97 @@ def convert_wig_to_dcr(wig, dcr, chunk_size=1000, **kwargs):
                 info.setdefault('start', '1')
                 info.setdefault('step', '1')
                 info.setdefault('span', '1')
-                chrom = info['chrom']
-                writed = 0
                 chrom_lines = 1
                 chunk_values = []
-
-                ohi.write('%s:%s\n' % (chrom, lines))
+                info.update({
+                        'mean': null_value,
+                        'min': null_value,
+                        'max': null_value,
+                        'total': 0,
+                        'totals': 0,
+                        'writed': 0,
+                        'first_line': lines})
 
                 start = int(info['start'])
                 if start > 0:
                     for i in range(1, start):
-                        chunk_values.append(null_value)
-                        writed += 1
-                        if writed % chunk_size == 0:
-                            first_num = (chrom_lines - 1) * chunk_size + 1
-                            last_num = (chrom_lines) * chunk_size
-                            oh.write('\n%s\t%s\t%s\t' %
-                                     (chrom, first_num, last_num))
-                            for value in chunk_values[:-1]:
-                                oh.write('%s ' % value)
-                            oh.write('%s' % chunk_values[-1])
-                            lines += 1
-                            writed = 0
-                            chunk_values = []
-                            chrom_lines += 1
+                        info = collect_info(info, null_value, conv)
+                        chrom_lines, chunk_values, lines, info = \
+                                process_value(chrom_lines, chunk_values,
+                                              lines, info, oh)
             else:
                 chunk_values.append(line)
-                writed += 1
-                if writed % chunk_size == 0:
-                    first_num = (chrom_lines - 1) * chunk_size + 1
-                    last_num = (chrom_lines) * chunk_size
-                    oh.write('\n%s\t%s\t%s\t' % (chrom, first_num, last_num))
-                    for value in chunk_values[:-1]:
-                        oh.write('%s ' % value)
-                    lines += 1
-                    oh.write('%s' % chunk_values[-1])
-                    writed = 0
-                    chunk_values = []
-                    chrom_lines += 1
+                info = collect_info(info, line, conv)
+                chrom_lines, chunk_values, lines, info = \
+                        process_value(chrom_lines, chunk_values,
+                                      lines, info, oh)
     if chunk_values:
-        first_num = (chrom_lines - 1) * chunk_size + 1
-        last_num = first_num + len(chunk_values) - 1
-        oh.write('\n%s\t%s\t%s\t' % (chrom, first_num, last_num))
-        lines += 1
-        for value in chunk_values[:-1]:
-            oh.write('%s ' % value)
-        oh.write('%s' % chunk_values[-1])
+        info = collect_info(info, line, conv)
+        chrom_lines, chunk_values, lines, info = \
+                process_value(chrom_lines, chunk_values,
+                              lines, info, oh)
+    if 'chrom' in info:
+        write_index(info, ohi)
 
     ih.close()
     oh.close()
     ohi.close()
 
 
+def collect_info(info, line, conv):
+    num = conv(line)
+    if num < info['min']:
+        info['min'] = num
+    if num > info['max']:
+        info['max'] = num
+    info['totals'] += 1
+    info['total'] += num
+    info['writed'] += 1
+    return info
+
+
+def process_value(chrom_lines, chunk_values, lines, info, oh):
+    if info['writed'] % chunk_size == 0:
+        first_num = (chrom_lines - 1) * chunk_size + 1
+        last_num = (chrom_lines) * chunk_size
+        oh.write('\n%s\t%s\t%s\t' % (info['chrom'], first_num, last_num))
+        for value in chunk_values[:-1]:
+            oh.write('%s ' % value)
+        lines += 1
+        oh.write('%s' % chunk_values[-1])
+        info['writed'] = 0
+        chunk_values = []
+        chrom_lines += 1
+    return chrom_lines, chunk_values, lines, info
+
+
+def write_index(info, ohi):
+    ohi.write('%s:%s:tot=%s;min=%s;max=%s;mean=%.4f\n' %
+              (info['chrom'], info['first_line'],
+               info['totals'], info['min'], info['max'],
+               info['total'] / float(info['totals'])))
+
 if __name__ == "__main__":
     chunk_size = 1000
     separator = ' '
-    types = 'int'
-    null_value = 0
 
     try:
         wig = sys.argv[1]
         dcr = sys.argv[2]
     except:
-        print 'Usage: \n\tpython convert_wig_to_dcr.py <wig> <dcr>'
+        print 'Usage: \n\tpython convert_wig_to_dcr.py <wig> <dcr> ' + \
+                '[<types> <null_value]'
+
+    try:
+        types = sys.argv[3]
+        null_value = sys.argv[4]
+        if types == 'int':
+            null_value = int(null_value)
+        if types == 'float':
+            null_value = float(null_value)
+    except:
+        types = 'int'
+        null_value = 0
 
     sys.stderr.write("Version B\n")
     data = {
